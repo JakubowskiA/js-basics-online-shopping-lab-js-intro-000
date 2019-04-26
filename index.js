51757fc161c9b3af16374f0ab1c0a045127bb77b
Ariel J <arieljakubowski@gmail.com>
2019-04-26 15:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for(let i = 0; i<cart.length;i++){
-    if(cart[i] === item){
-cart = [...cart.slice(0,i),...cart.slice(i)];
+    if(cart[i].itemName === item){
+cart = [...cart.slice(0,i),...cart.slice(i+1)];
 return cart;
     }
     }
